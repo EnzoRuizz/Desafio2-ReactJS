2f79a59e00bfd77de39c023d67f3776d88c19b0b
EnzoRuizz <renzomach56@gmail.com>
2021-12-06 21:09:34 -0300

    Second commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,39 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img className="logo" src="logo192.png" alt="logo"/></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Categorias
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Tecnologia</a></li>
-                  <li><a class="dropdown-item" href="#">Hogar</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Juegos y juguetes</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Ayuda</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
-              <button class="btn btn-primary" type="submit">Buscar</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+            <Navbar.Brand href="#home">
+            <img
+              alt="Logo"
+              src="logo192.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Inicio</Nav.Link>
+              <Nav.Link href="#link">Ayuda</Nav.Link>
+              <NavDropdown title="Categorias" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Tecnologia</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Hogar</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Juegos y juguetes</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Libros</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     )
 }
 
