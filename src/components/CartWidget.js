8c9f4ca1753055fe34747fe5816cf51a3abb5908
EnzoRuizz <renzomach56@gmail.com>
2021-12-08 19:35:22 -0300

    Correcion

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -5,7 +5,10 @@ import '../styles/CartWidget.css';
 function CartWidget() {
     return (
         <div>
-            <Nav.Link><i className="fas fa-shopping-cart fa-2x"></i></Nav.Link>
+            <Nav.Link>
+                <i className="fas fa-shopping-cart fa-2x"></i>
+                <span className="cant">0</span>
+            </Nav.Link>
         </div>
     )
 }
