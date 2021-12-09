44a6751ec40087e96e2f6816c49d53b9e51b0cb4
EnzoRuizz <86387396+EnzoRuizz@users.noreply.github.com>
2021-12-08 21:17:00 -0300

    Cambio en el component itemListContainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-/* 
+
 function Productos(props){
     return <h1 className="text-center my-3">{props.bienvenida}</h1>
 }
@@ -12,12 +12,13 @@ function ItemListContainer() {
             <Productos bienvenida={texto}/>
         </div>
     )
-} */
+}
 
+/*
 const Texto = <h1 className="text-center my-3">Productos</h1>
 
 function ItemListContainer() {
     return (Texto)
-}
+} */
 
 export default ItemListContainer;
