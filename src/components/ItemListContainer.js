09d8790e191a490aff084c7894ec6e4770513fc5
EnzoRuizz <renzomach56@gmail.com>
2021-12-13 20:40:05 -0300

    Desafio 4

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import ItemCount from './ItemCount';
-=======
->>>>>>> 44a6751ec40087e96e2f6816c49d53b9e51b0cb4
 
 function Productos(props){
     return <h1 className="text-center my-3">{props.bienvenida}</h1>
@@ -18,12 +15,7 @@ function ItemListContainer() {
         </div>
     )
 }
-<<<<<<< HEAD
 /* 
-=======
-
-/*
->>>>>>> 44a6751ec40087e96e2f6816c49d53b9e51b0cb4
 const Texto = <h1 className="text-center my-3">Productos</h1>
 
 function ItemListContainer() {
